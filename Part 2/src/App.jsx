9d7fa12fc767546c3d99ab1de107717288276d23
Blaez <caugustin14@gmail.com>
2024-03-23 16:30:55 -0400

    Finished 2.5

--- Part 2/src/App.jsx
+++ Part 2/src/App.jsx
@@ -1,47 +1,70 @@
 const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
-  const sum = course.parts.reduce((s, p) => 
+  const sum = 
+  
+  courses[0].parts.reduce((s, p) => 
     s + p.exercises, 0, 
   )
 
   const Course = ({ nomen }) => {
     return (
-          nomen.parts.map(courses => <p key={courses.id}>
-            {courses.name} {courses.exercises}</p>)
-          )
-  }
+          nomen.map(coursearbi => <div key={coursearbi.id}>
+            <h1>{coursearbi.name}</h1>
+            {coursearbi.parts.map((item, index) => (
+              <p key={`item-${index}`}>{item.name} {item.exercises}
+              </p>
+            ))}
+            <b>total of {coursearbi.parts.reduce((s,p) => 
+            s + p.exercises, 0,)} exercises</b>
+            </div>
+            ))}
   return (
   <div>
-    <h1>{course.name}</h1>
-    <Course nomen={course} />
-    <b>total of {sum} exercises</b>
+    <Course nomen={courses} />
   </div>
   )
 }
